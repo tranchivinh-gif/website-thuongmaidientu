00e316af7e9a9d225cc984f41d6bd65b676f68d1
unknown <vinhngungoc69@gmail.com>
2026-06-11 18:55:45 +0700

    caỉ thiện lại chức năng đăng nhập, phân biệt vai trò người dùng, chỉnh sửa cấu trúc sạch hơn

--- src/view/vchangepassword.php
+++ src/view/vchangepassword.php
@@ -1,4 +1,4 @@
-<form action="" method="POST">
+<form action="?page=vchangepassword" method="POST">
     <table>
         <tr>
             <td>Nhập mật khẩu mới</td>
@@ -14,32 +14,3 @@
         </tr>
     </table>
 </form>
-
-<?php
-if (isset($_POST["btnchange"])) {
-    include_once __DIR__ . "/../controller/UserCtrl.php";
-    $userCtrl = new UserCtrl();
-
-    $resultconfirm = $userCtrl->confirmPassword($_POST["txtpassword"], $_POST["txtrepassword"]);
-    if (!$resultconfirm) {
-        echo '<script>
-            alert("Mật khẩu không khớp!");
-          </script>';
-        exit();
-    }
-
-    $resultchange = $userCtrl->changePassword($_SESSION["user"]["UserID"], $_POST["txtpassword"]);
-    if (!$resultchange) {
-        echo '<script>
-            alert("Đổi mật khẩu thất bại!");
-          </script>';
-        exit();
-    } else {
-        echo '<script>
-            alert("Đổi mật khẩu thành công!");
-            window.location.href="?employee";
-          </script>';
-        exit();
-    }
-}
-?>
---
+++ src/view/vhome.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="header">
+            <div class="nav">
+                <ul>
+                    <?php
+                    displayMenu();
+                    ?>
+                </ul>
+            </div>
+            <div class="search">
+                <form action="" method="GET">
+                    <input type="search" name="txtsearch">
+                    <button name="btnsearch">Tìm kiếm</button>
+                </form>
+            </div>
+        </div>
+        <div class="section">
+            <div class="left"></div>
+            <div class="right">
+                <?php
+                router();
+                ?>
+            </div>
+        </div>
+        <div class="footer">TRẦN CHÍ VĨNH</div>
+    </div>
+</body>
+
+</html>
